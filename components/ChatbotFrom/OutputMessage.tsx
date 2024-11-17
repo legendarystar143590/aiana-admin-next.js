8fa01efdb9a1870f99573e298402f81f186ff24f
Aleksandr Buzuev <legendarystar2160187@gmail.com>
2024-11-17 11:08:54 +0900

    updated:ChatbotForm

---
+++ components/ChatbotFrom/OutputMessage.tsx
@@ -0,0 +1,32 @@
+import Avatar from "../Avatar"
+const OutputMessage = ({ message, avatarPreview, key }) => 
+  <div
+    key={key}
+    className={`p-2 flex gap-3 ${message.isBot ? "" : "justify-end"}`}
+    style={{
+      maxWidth: "90%",
+      alignSelf: message.isBot ? "flex-start" : "flex-end",
+    }}
+  >
+    <Avatar
+      src={message.isBot ? avatarPreview || "/images/logo_short_black.png" : "/images/logo_short_black.png"}
+      name="avatar"
+      className={`rounded-full size-12 ${!message.isBot && "hidden"}`}
+    />
+
+    <div
+      className={`flex gap-2 p-2 rounded-lg break-words ${
+        message.isBot
+          ? "bg-[#EBEBEB] text-[#070E0B]"
+          : "flex-row-reverse bg-[#A536FA] text-white"
+      }`}
+    >
+      <div
+        className="flex-grow"
+        style={{ textAlign: message.isBot ? "left" : "right", overflowWrap: "break-word" }}
+        dangerouslySetInnerHTML={{ __html: message.text }}
+      />
+    </div>
+  </div>
+
+export default OutputMessage
